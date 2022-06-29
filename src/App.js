cfc25a5dc9c4fc5ab617b9a17fc5886f6e6f368e
Juan Musante <juanpedro.musante@gmail.com>
2022-06-29 01:31:38 -0300

    Agregado de HEADER, con modificacion de estilos.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hola MUNDO!! 
+        <p className="logo">TATTOO $H0P</p>
+        <nav className="navegador">
+            <ul className="ul">
+                <li className="li">
+                    <p>TIENDA</p>
+                </li>
+                <li className="li">
+                    <p>CARRITO</p>
+                </li>
+            </ul>
+        </nav>
       </header>
     </div>
   );
