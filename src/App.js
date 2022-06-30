ad7a5ccf7305c484b43f1d3dd25433301238f314
Juan Musante <juanpedro.musante@gmail.com>
2022-06-29 22:42:49 -0300

    Se agrego la carpeta components y dentro de la misma se armo el archivo JS NavBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p className="logo">TATTOO $H0P</p>
-        <nav className="navegador">
-            <ul className="ul">
-                <li className="li">
-                    <p>TIENDA</p>
-                </li>
-                <li className="li">
-                    <p>CARRITO</p>
-                </li>
-            </ul>
-        </nav>
-      </header>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+function NavBar(){
+    return(
+        <header className="App-header">
+            <p className="logo">TATT00 SH0P</p>
+            <nav className="navegador">
+                <ul className="ul">
+                    <li className="li">
+                        <a href="filtro1.html">FILTRO 1</a>
+                    </li>
+                    <li className="li">
+                        <a href="filtro2.html">FILTRO 2</a>
+                    </li>
+                    <li className="li">
+                        <a href="filtro3.html">FILTRO 3</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
